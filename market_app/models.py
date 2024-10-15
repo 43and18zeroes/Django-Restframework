0c3eb3b26387216ff61fd52c8a55ddea1dbc91b2
Christoph <c.wagner.germany@gmail.com>
2024-10-15 14:47:04 +0200

    net1

--- market_app/models.py
+++ market_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Market(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    description = models.TextField()
+    net_worth = models.DecimalField(max_digits=100, decimal_places=2)
+    
+    def __str__(self):
+        return self.name
