c0570024f57f99c95d73daefb8c9dc12a3045297
Christoph <c.wagner.germany@gmail.com>
2024-10-16 16:52:31 +0200

    ser5

--- market_app/api/serializers.py
+++ market_app/api/serializers.py
@@ -1,10 +1,23 @@
 from rest_framework import serializers
 from market_app.models import Market
 
+def validate_no_x(value):
+    errors = []
+    
+    if 'X' in value:
+        errors.append('no X in location')
+    if 'Y' in value:
+        errors.append('no Y in location')
+        
+    if errors:
+        raise serializers.ValidationError(errors)
+    
+    return value
+
 class MarketSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(max_length=255)
-    location = serializers.CharField(max_length=255)
+    location = serializers.CharField(max_length=255, validators=[validate_no_x])
     description = serializers.CharField()
     net_worth = serializers.DecimalField(max_digits=100, decimal_places=2)
     
@@ -18,8 +31,3 @@ class MarketSerializer(serializers.Serializer):
         instance.net_worth = validated_data.get('net_worth', instance.net_worth)
         instance.save()
         return instance
-    
-    def validate_location(self, value):
-        if 'X' in value:
-            raise serializers.ValidationError('no X in location')
-        return value
