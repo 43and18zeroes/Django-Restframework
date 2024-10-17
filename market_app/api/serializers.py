7cb2b5d393eacd7553a7afca69c17616d16684d8
Christoph <c.wagner.germany@gmail.com>
2024-10-17 15:37:15 +0200

    val1

--- market_app/api/serializers.py
+++ market_app/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from market_app.models import Market
+from market_app.models import Market, Seller
 
 def validate_no_x(value):
     errors = []
@@ -31,3 +31,33 @@ class MarketSerializer(serializers.Serializer):
         instance.net_worth = validated_data.get('net_worth', instance.net_worth)
         instance.save()
         return instance
+    
+class SellerDetailSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=255)
+    contact_info = serializers.CharField()
+    markets = MarketSerializer(many=True, read_only=True)
+    
+class SellerCreateSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=255)
+    contact_info = serializers.CharField()
+    markets = serializers.ListField(child=serializers.IntegerField(), write_only=True)
+    
+    def validate_markets(self, value):
+        markets = Market.objects.filter(id__in=value)
+        if len(markets) != len(value):
+            raise serializers.ValidationError("Passt nicht mit den IDs")
+        return value
+    
+    def create(self, validated_data):
+        market_ids = validated_data.pop('markets')
+        seller = Seller.objects.create(**validated_data)
+        markets = Market.objects.filter(id__in=market_ids)
+        seller.markets.set(markets)
+        return seller
+    
+# {
+#     "name": "Seller1",
+#     "contact_info": "seller1@gmail.com",
+#     "markets": "[2]"
+# }
