458e38459b248d4b1766f3050dc58fb9ae800f2d
Christoph <c.wagner.germany@gmail.com>
2024-10-19 15:52:48 +0200

    ser7

--- market_app/api/serializers.py
+++ market_app/api/serializers.py
@@ -4,7 +4,7 @@ from market_app.models import Market, Seller, Product
 class MarketSerializer(serializers.ModelSerializer):
     class Meta:
         model = Market
-        fields = '__all__'
+        fields = ['id', 'name', 'location', 'description', 'net_worth']
         
     def validate_name(self, value):
         errors = []
