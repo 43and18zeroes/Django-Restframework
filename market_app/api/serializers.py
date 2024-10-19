117608d564cd2e5b9052067188f2dc1c78d83067
Christoph <c.wagner.germany@gmail.com>
2024-10-19 15:50:48 +0200

    ser6

--- market_app/api/serializers.py
+++ market_app/api/serializers.py
@@ -1,40 +1,23 @@
 from rest_framework import serializers
 from market_app.models import Market, Seller, Product
 
-def validate_no_x(value):
-    errors = []
-    
-    if 'X' in value:
-        errors.append('no X in location')
-    if 'Y' in value:
-        errors.append('no Y in location')
-        
-    if errors:
-        raise serializers.ValidationError(errors)
-    
-    return value
-
 class MarketSerializer(serializers.ModelSerializer):
-    # id = serializers.IntegerField(read_only=True)
-    # name = serializers.CharField(max_length=255)
-    # location = serializers.CharField(max_length=255)
-    # description = serializers.CharField()
-    # net_worth = serializers.DecimalField(max_digits=100, decimal_places=2)
-    
-    # def create(self, validated_data):
-    #     return Market.objects.create(**validated_data)
-    
-    # def update(self, instance, validated_data):
-    #     instance.name = validated_data.get('name', instance.name)
-    #     instance.location = validated_data.get('location', instance.location)
-    #     instance.description = validated_data.get('description', instance.description)
-    #     instance.net_worth = validated_data.get('net_worth', instance.net_worth)
-    #     instance.save()
-    #     return instance
-    
     class Meta:
         model = Market
         fields = '__all__'
+        
+    def validate_name(self, value):
+        errors = []
+    
+        if 'X' in value:
+            errors.append('no X in location')
+        if 'Y' in value:
+            errors.append('no Y in location')
+            
+        if errors:
+            raise serializers.ValidationError(errors)
+        
+        return value
     
 class SellerDetailSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
