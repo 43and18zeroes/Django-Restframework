320d33b34a0ee6b9b94ce38fa7f20552d5d97299
Christoph <c.wagner.germany@gmail.com>
2024-10-22 16:32:04 +0200

    mix7

--- market_app/api/views.py
+++ market_app/api/views.py
@@ -19,21 +19,26 @@ class MarketsView(generics.ListAPIView):
     #     return self.create(request, *args, **kwargs)
 
 
-class MarketsSingleView(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class MarketsSingleView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Market.objects.all()
     serializer_class = MarketSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
 
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
+# class MarketsSingleView(mixins.RetrieveModelMixin,
+#                     mixins.UpdateModelMixin,
+#                     mixins.DestroyModelMixin,
+#                     generics.GenericAPIView):
+#     queryset = Market.objects.all()
+#     serializer_class = MarketSerializer
 
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
+#     def get(self, request, *args, **kwargs):
+#         return self.retrieve(request, *args, **kwargs)
+
+#     def put(self, request, *args, **kwargs):
+#         return self.update(request, *args, **kwargs)
+
+#     def delete(self, request, *args, **kwargs):
+#         return self.destroy(request, *args, **kwargs)
 
 
 class SellerView(mixins.ListModelMixin,
