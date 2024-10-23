ff1677bdb001c7c4652eccc017b2153057e06df1
Christoph <c.wagner.germany@gmail.com>
2024-10-23 15:34:42 +0200

    mod1

--- market_app/api/serializers.py
+++ market_app/api/serializers.py
@@ -2,18 +2,13 @@ from rest_framework import serializers
 from market_app.models import Market, Seller, Product
 
 class MarketSerializer(serializers.ModelSerializer):
-    
-    # sellers = serializers.StringRelatedField(many=True, read_only=True)
-    
+     
     def __init__(self, *args, **kwargs):
-        # Don't pass the 'fields' arg up to the superclass
         fields = kwargs.pop('fields', None)
 
-        # Instantiate the superclass normally
         super().__init__(*args, **kwargs)
 
         if fields is not None:
-            # Drop any fields that are not specified in the `fields` argument.
             allowed = set(fields)
             existing = set(self.fields)
             for field_name in existing - allowed:
@@ -66,71 +61,16 @@ class ProductDetailSerializer(serializers.ModelSerializer):
         model = Product
         fields = '__all__'
         
-    # name = serializers.CharField(max_length=255)
-    # description = serializers.CharField()
-    # price = serializers.DecimalField(max_digits=50, decimal_places=2)
-    
-    # market_display = serializers.CharField(source='market.name', read_only=True) 
-    # seller_display = serializers.CharField(source='seller.name', read_only=True) 
-
-    # market = serializers.IntegerField(write_only=True)
-    # seller = serializers.IntegerField(write_only=True)
-    
-    # def update(self, instance, validated_data):
-    #     instance.name = validated_data.get('name', instance.name)
-    #     instance.description = validated_data.get('description', instance.description)
-    #     instance.price = validated_data.get('price', instance.price)
-        
-    #     if 'market' in validated_data:
-    #         try:
-    #             market = Market.objects.get(id=validated_data['market'])
-    #             instance.market = market
-    #         except Market.DoesNotExist:
-    #             raise serializers.ValidationError("Market ID does not exist")
-        
-    #     if 'seller' in validated_data:
-    #         try:
-    #             seller = Seller.objects.get(id=validated_data['seller'])
-    #             instance.seller = seller
-    #         except Seller.DoesNotExist:
-    #             raise serializers.ValidationError("Seller ID does not exist")
-        
-    #     instance.save()
-    #     return instance
     
 class ProductCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
-        
-    # name = serializers.CharField(max_length=255)
-    # description = serializers.CharField()
-    # price = serializers.DecimalField(max_digits=50, decimal_places=2)
-    # market = serializers.IntegerField(write_only=True)
-    # seller = serializers.IntegerField(write_only=True)
-    
-    # def validate_market(self, value):
-    #     try:
-    #         Market.objects.get(id=value)
-    #     except Market.DoesNotExist:
-    #         raise serializers.ValidationError("Market ID does not exist")
-    #     return value
-
-    # def validate_seller(self, value):
-    #     try:
-    #         Seller.objects.get(id=value)
-    #     except Seller.DoesNotExist:
-    #         raise serializers.ValidationError("Seller ID does not exist")
-    #     return value
-
-    # def create(self, validated_data):
-    #     market_id = validated_data.pop('market')
-    #     seller_id = validated_data.pop('seller')
-    #     market = Market.objects.get(id=market_id)
-    #     seller = Seller.objects.get(id=seller_id)
-    #     product = Product.objects.create(market=market, seller=seller, **validated_data)
-    #     return product
-    
+   
+   
+class SellerListSerializer(SellerSerializer):
+    class Meta:
+        model = Seller       
     
 """
 Seller:
